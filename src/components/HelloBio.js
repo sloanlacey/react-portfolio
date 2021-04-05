832850d3c2899d3fc6a31cc09bd9684c7f5aae66
sloanlacey <sloanlacey89@gmail.com>
2021-04-04 17:31:57 -0700

    Create cohesive jumbotron with profile image and basic card setup for bio.

--- src/components/HelloBio.js
+++ src/components/HelloBio.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { Jumbotron, Card } from 'react-bootstrap';
 import '../styles/HelloBio.css';
 
 
 function HelloBio() {
     return (
-        <div className='header'>
+        <>
+        <Jumbotron fluid className='header'>
+            
+        {/* <div className='header'> */}
             <div className='row'>
-                <div id='profile-image' className='col-lg-6 col-md-12 col-sm-12'>
-                    <p className='circle'></p>
-                </div>
-                <div className='col-lg-6 col-md-12 col-sm-12'>
-                    2 of 2
-                </div>
+                 <div id='profile-image' className='col-lg-6 col-md-12 col-sm-12'>
+                     <p className='circle'></p>
+                 </div>
+                 <div className='col-lg-6 col-md-12 col-sm-12 bio-card'>
+                    <Card className='bio-content' style={{ width: '18rem' }}>
+                        <Card.Header>UI/UX Design Wizard</Card.Header>
+                            <Card.Body>
+                            <Card.Title>Social Links</Card.Title>
+                            <Card.Text>
+                                Social links, button to open popout with bio.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+               </div>
             </div>
-        </div>
+        {/* </div> */}
+        </Jumbotron>
+        </>
+        // <div className='header'>
+        //     <div className='row'>
+        //         <div id='profile-image' className='col-lg-6 col-md-12 col-sm-12'>
+        //             <p className='circle'></p>
+        //         </div>
+        //         <div className='col-lg-6 col-md-12 col-sm-12'>
+        //             2 of 2
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
